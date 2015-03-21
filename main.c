35924b8ec32bcaa1396d31b1aa2449f5abfa89d9
Fabio <fabio.luccon@hotmail.it>
2015-03-21 12:10:04 +0100

    Update main.c
    Documentazione del codice ed eliminazione parti superflue

--- main.c
+++ main.c
@@ -20,12 +20,12 @@
 #define N 10
 
 int buffer[N];
-int in,out; //indici scrittura e lettura
-int full; //buffer pieno
-int empty; //buffer vuoto
+int in,out; 																				//indici scrittura e lettura
+int full; 																					//buffer pieno
+int empty; 																					//buffer vuoto
 
-CRITICAL_SECTION mutex; //semaforo binario
-CONDITION_VARIABLE not_full;// variabili condizione
+CRITICAL_SECTION mutex; 																	//semaforo binario
+CONDITION_VARIABLE not_full;																// variabili condizione
 CONDITION_VARIABLE not_empty;
 
 unsigned long WINAPI Produce_thread(void* arg);
@@ -40,37 +40,37 @@ int main()
 	srand(time(NULL));
 	printf("Produttore e Consumatore WIN\n");
 	printf("We have 2 thread one produce other read from buffer\n");	
-    full=FALSE; //buffer pieno NO
-    empty=TRUE; //buffer vuoto SI
-    in=0; //indice di scrittura sul buffer  
-    out=0; //indice di lettura dal buffer
+    full=FALSE; 																			//buffer pieno NO
+    empty=TRUE; 																			//buffer vuoto SI
+    in=0; 																					//indice di scrittura sul buffer  
+    out=0; 																					//indice di lettura dal buffer
     InitializeCriticalSection(&mutex);
-    // creazione dei thread corrispondenti alle funzioni di lettura, elaborazione e scrittura
+   	 																						// creazione dei thread corrispondenti alle funzioni di lettura, elaborazione e scrittura
     P_thread = CreateThread(NULL, 1024, &Produce_thread, NULL, 0, NULL);
     C_thread = CreateThread(NULL, 1024, &Consume_thread, NULL, 0, NULL);
-    // attesa della terminazione dei thread
+    																						// attesa della terminazione dei thread
     WaitForSingleObject(P_thread, INFINITE);
     WaitForSingleObject(C_thread, INFINITE);
     printf("\n...END\n");
     return 0;
 }
-unsigned long WINAPI Produce_thread(void* arg) // Produttore
+unsigned long WINAPI Produce_thread(void* arg) 												// Produttore
 {	int r; int i;
 	for(i=0;i<100;i++){
 	    r=RandomNumber(1,6);	    	    
-        //while(full) printf("<P>"); //busy waiting 
+        																					//while(full) printf("<P>"); //busy waiting 
 EnterCriticalSection(&mutex);
         if (full) 
 		{ printf("<PS>");
 		  SleepConditionVariableCS(&not_full, &mutex, INFINITE);
 		}
 LeaveCriticalSection(&mutex);
-        //delay(RandomNumber(1,3)*10);
-		buffer[in]=r; //scrivo sul buffer
+        																					//delay(RandomNumber(1,3)*10);
+		buffer[in]=r; 																		//scrivo sul buffer
 		in=(in+1)%N;
 		printf("-P:%d",r);
 EnterCriticalSection(&mutex);
-	    if (in==out) full=TRUE; //segnalo anche che ora il buffer è pieno
+	    if (in==out) full=TRUE;															 	//segnalo anche che ora il buffer è pieno
         if (empty) empty=FALSE;
 LeaveCriticalSection(&mutex);
         WakeConditionVariable(&not_empty);
@@ -78,11 +78,11 @@ LeaveCriticalSection(&mutex);
 	ExitThread(0);
 }
 
-unsigned long WINAPI Consume_thread(void* arg) // Consumatore
-{	int r; int i;
+	unsigned long WINAPI Consume_thread(void* arg) 											// Consumatore
+	{	int r; int i;
 	for(i=0;i<100;i++){
 		delay(RandomNumber(1,3)*10);    
-      //  while(!full) printf("<V>"); //busy waiting 
+      																						//  while(!full) printf("<V>"); //busy waiting 
 EnterCriticalSection(&mutex);
         if (empty) 
 		  {
@@ -91,7 +91,7 @@ EnterCriticalSection(&mutex);
 		  }
 		  
 LeaveCriticalSection(&mutex);
-		r=buffer[out];//leggo dal buffer
+		r=buffer[out];																		//leggo dal buffer
 		out=(out+1)%N;
         printf("-C:%d\n",r);
 EnterCriticalSection(&mutex);
@@ -109,72 +109,4 @@ int RandomNumber(int from, int to)
 	
 	return r;
 }
-
-/*N=100000;
-thread P()
-
-thred C()
-
-main()
-{
-   empty=TRUE;
-   full=FALSE;
-   count=0;
-   esegui il thread P
-   esegui il thread C
-   aspetta la fine di P
-   aspetta la fine di C
-   return 0;
-}
-
-thread P()
- total=1000;
- do
-{ 
-
-  if (full) sleep (not_full);
-   1. full falsa
-     2. full vera ho dormito
-            e poi qualcuno mi
-            ha risvegliato.
-  
-  r=random;
-  total--
-  delay(variabile fra 0.1 e 5 sec)
-  b[i];
-  i++;
-  count++;
-  if !(count<N) full=TRUE;
-  if (empty==TRUE) 
-     empty=FALSE;
-     wake(not_empty)
-
-  if total==0 continua=FALSE
- }while(continua) 
-  
- if(empty==TRUE)
-    wake(not_empty)
-  exit
- 
-
-
-thred C()
-
- do{
-   if(empty) sleep(not_empty)
-   stampa b[j]
-   delay(variabile fra 0.1 e 5 sec)
-   j++
-   count--
-   if (count==0) 
-      empty=TRUE
-      if continua==FALSE
-         consumatore fuori!
-   if (full==TRUE) 
-       full=FALSE   
-       wake(not_full)
-  }while(1)
-
-*/
-
 
